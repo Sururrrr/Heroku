2b0d41324f86aa70c88c1ffe6e06f7d071298f33
Sururrrr <sourour.cherif@esprit.tn>
2022-08-03 14:15:29 +0100

    ImageClassifier Deployed on Heroku

--- app.py
+++ app.py
@@ -1,34 +1,66 @@
-import numpy as np
-import pandas as pd
-from flask import Flask, request, render_template
-from sklearn import preprocessing
-import pickle
 
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-cols=['age','workclass','education','marital-status','occupation','relationship','race','gender','capital-gain','capital-loss',
-      'hours-per-week','native-country']
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
-@app.route('/')
+from keras.applications.mobilenet import MobileNet
+from keras.applications.mobilenet import preprocess_input
+from keras.applications.mobilenet import decode_predictions
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from utils.helper_functions import allowed_file
+
+UPLOAD_FOLDER = 'images'
+saved_model = 'model/model.h5'
+if os.path.exists(saved_model):
+    model = load_model(saved_model)
+else:
+    model = MobileNet()
+    model.save(saved_model)
+
+app = Flask(__name__, template_folder='templates')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    feature_list = request.form.to_dict()
-    feature_list = list(feature_list.values())
-    feature_list = list(map(int, feature_list))
-    final_features = np.array(feature_list).reshape(1, 12) 
-    
-    prediction = model.predict(final_features)
-    output = int(prediction[0])
-    if output == 1:
-        text = ">50K"
+    """The user is provided with an interface where
+    he/she can upload an image. After submission,
+    a deep learning model is used to make a prediction
+    of what the image could be. The result is then sent to
+    the user.
+    """
+    # get file
+    file = request.files['imagefile']
+    filename = secure_filename(file.filename)
+
+    if file is None or file.filename == "":
+        out_message = "No file"
+    elif not allowed_file(file.filename):
+        out_message = "Format not currently supported"
     else:
-        text = "<=50K"
+        # save image in database
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename).replace("\\","/")
+        file.save(image_path)
 
-    return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
+        # load & preprocess image
+        image = load_img(image_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        image = preprocess_input(image)
+        try:
+            # predict
+            y_pred = model.predict(image)
+            label = decode_predictions(y_pred)
+            label = label[0][0]
+            out_message = "Your image is a %s (%.2f%%)" %(label[1], label[2]*100)
+        except:
+            out_message = "An error occured during prediction"
 
+    return render_template('index.html', out_message=out_message)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
